071bb4fb74bd5bbcfb702ecde993b12fcb925924
cprachaseree <jasoncprach@gmail.com>
2020-03-23 18:55:05 +0800

    styled login page

--- travapotami/auth.py
+++ travapotami/auth.py
@@ -6,3 +6,13 @@ auth_blueprint = Blueprint('auth_blueprint', __name__)
 def login():
     # flask automatically enter templates directory when render_template
     return render_template('./auth/login.html', title='Login')
+
+@auth_blueprint.route('/register')
+def register():
+    # flask automatically enter templates directory when render_template
+    return render_template('./auth/register.html', title='Register')
+
+@auth_blueprint.route('/logout')
+def logout():
+    # flask automatically enter templates directory when render_template
+    return render_template('./auth/logout.html', title='Login')
