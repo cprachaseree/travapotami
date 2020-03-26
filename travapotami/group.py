36be86dae18453f33ef8fd8623615f6b4e815e0f
pareeyaporn <46850740+pareeyaporn@users.noreply.github.com>
2020-03-27 01:46:18 +0700

    added more group pages and home page styling

--- travapotami/group.py
+++ travapotami/group.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template, request, flash
 
 group_blueprint = Blueprint('group_blueprint', __name__)
 
+@group_blueprint.route('/choose_group')
+def choose_group():
+    return render_template('./group/choose_group.html', title='Choose Group')
+
 @group_blueprint.route('/create_group', methods=['GET', 'POST'])
 def create_group():
     if request.method == 'POST':
@@ -23,3 +27,15 @@ def create_group():
         # TODO find userid of all users with the given username and add to the group
         return render_template('./group/create_group.html')
     return render_template('./group/create_group.html', title='Create Group')
+
+@group_blueprint.route('/edit_group')
+def edit_group():
+    return render_template('./group/edit_group.html', title='Edit Group')
+
+@group_blueprint.route('/browse_groups')
+def browse_groups():
+    return render_template('./group/browse_groups.html', title='Browse Groups')
+
+@group_blueprint.route('/my_groups')
+def my_groups():
+    return render_template('./group/my_groups.html', title='My Groups')
