2bce52fcca9f14e0b68560593f70f637d5f63cec
pareeyaporn <46850740+pareeyaporn@users.noreply.github.com>
2020-04-22 20:05:06 +0700

    add edit_group and other group pages

--- travapotami/group.py
+++ travapotami/group.py
@@ -12,9 +12,73 @@ def choose_group():
 
 @group_blueprint.route('/create_group', methods=['GET', 'POST'])
 def create_group():
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            group_name = request.form['groupname']
+            public = request.form.get('accessibility')
+            if public:
+                public = True
+                flash("Public Group")
+            else:
+                public = False
+                flash("Private Group")
+            usernames = []
+            # get usernames from the form
+            for x in request.form:
+                if x == "groupname" or not request.form[x] or request.form[x] == 'on':
+                    continue
+                usernames.append(request.form[x])
+            #flash(usernames)
+
+            group = Group( group_name = group_name,
+                            public = public
+            )
+            db.session.add(group)
+            db.session.commit()
+            group.admins = [current_user]
+            db.session.commit()
+
+            to_add = []
+            for username in usernames: 
+                user = User.query.filter_by(username=username).first()
+                if user != None:
+                    to_add.append(user)
+                    flash(f"{username} added")
+                else:
+                    flash(f"{username} does not exist!")
+            group.mates = to_add
+            db.session.commit()
+
+            flash(f"{group_name} is created with {len(usernames)+1} member(s).")
+            return render_template('./group/create_group.html')
+
+        return render_template('./group/create_group.html', title='Create Group')
+
+    else:
+        flash("Login first please!")
+        return redirect(url_for('auth_blueprint.login')) 
+
+@group_blueprint.route('/group_info/<string:group>')
+def group_info(group):
+    group = int(''.join(filter(str.isdigit, group)))
+    searchgroup = Group.query.filter_by(id=group).first()
+    group = Group.query.get(group)
+
+    if current_user == searchgroup.admins[0]:
+        is_admin = True
+    else:
+        is_admin = False
+    return render_template('./group/group_info.html', title='Group Info', group=group, is_admin=is_admin)
+
+@group_blueprint.route('/edit_group/<string:group>',  methods=['GET', 'POST'])
+def edit_group(group):
+    groupnum = int(''.join(filter(str.isdigit, group)))
+    group = Group.query.get(groupnum)
+
     if request.method == 'POST':
         group_name = request.form['groupname']
         public = request.form.get('accessibility')
+
         if public:
             public = True
             flash("Public Group")
@@ -22,69 +86,68 @@ def create_group():
             public = False
             flash("Private Group")
         usernames = []
-        # get usernames from the form
+
         for x in request.form:
             if x == "groupname" or not request.form[x] or request.form[x] == 'on':
                 continue
             usernames.append(request.form[x])
-        #flash(usernames)
-
-        group = Group( group_name = group_name,
-                        public = public
-        )
-        db.session.add(group)
-        db.session.commit()
-        group.admins = [current_user]
+        flash(usernames)
+        
+        group.group_name = group_name
+        group.public = public
         db.session.commit()
 
         to_add = []
+        group_search = Group.query.filter_by(id=groupnum).first()
         for username in usernames: 
-            user = User.query.filter_by(username=username).first()
-            if user != None:
-                to_add.append(user)
-                flash(f"{username} added")
-            else:
-                flash(f"{username} does not exist!")
+            user_search = User.query.filter_by(username=username).first()
+            if user_search != None:
+                if not user_search in group_search.mates or not user_search in group_search.admins:
+                    flash(user_search.username)
+                    to_add.append(user_search)
+                    flash(f"{user_search.username} added to group")
+                else:
+                    flash(f"{user_search.username} already in group!")
         group.mates = to_add
+        flash(f"{group_name} is edited to {len(to_add)+1} member(s).")
         db.session.commit()
 
-        flash(f"{group_name} is created with {len(usernames)+1} member(s).")
-        return render_template('./group/create_group.html')
-
-    return render_template('./group/create_group.html', title='Create Group')
-
-@group_blueprint.route('/group_info/<string:group>')
-def group_info(group):
-    group = int(''.join(filter(str.isdigit, group)))
-    group = Group.query.get(group)
-    return render_template('./group/group_info.html', title='Group Info', group=group)
-
-@group_blueprint.route('/edit_group/<string:group>')
-def edit_group(group):
-    group = int(''.join(filter(str.isdigit, group)))
-    group = Group.query.get(group)
+        return redirect(url_for('group_blueprint.my_groups'))
     return render_template('./group/edit_group.html', title='Edit Group', group=group)
 
 @group_blueprint.route('/browse_groups')
 def browse_groups():
-    result = Group.query.filter_by(public='0').all()
+    result = Group.query.filter_by(public='1').all()
     page = request.args.get(get_page_parameter(), type=int, default=1)
     pagination = Pagination(page=page, total=len(result), per_page=6)
     return render_template('./group/browse_groups.html', title='Browse Groups', result=result, pagination=pagination, page=page, per_page=6)
 
+@group_blueprint.route('/search_groups')
+def search_groups():
+
+    return render_template('./group/search_groups.html', title='Search Groups')
+
 @group_blueprint.route('/my_groups')
 def my_groups():
-    result = Group.query.filter(or_(Group.admins.contains(current_user), Group.mates.contains(current_user))).all()
-    return render_template('./group/my_groups.html', title='My Groups', result=result)
+    if current_user.is_authenticated:
+        result = Group.query.filter(or_(Group.admins.contains(current_user), Group.mates.contains(current_user))).all()
+        return render_template('./group/my_groups.html', title='My Groups', result=result)
+    else:
+        flash("Login first please!")
+        return redirect(url_for('auth_blueprint.login'))
 
 @group_blueprint.route('/join_group/<string:groupnum>')
 def join_group(groupnum):
     groupnum = int(''.join(filter(str.isdigit, groupnum)))
-    group = Group.query.get(groupnum)
-
-    group.mates = [current_user]
-    db.session.commit()
-    flash(f"Successfully Joined {group.group_name}")
+    group = Group.query.filter_by(id=groupnum).first()
+    if current_user == group.admins[0]:
+        flash("You are the admin of this group!")
+    elif current_user in group.mates:
+        flash("You are already one of the group mates.")
+    else:
+        group.mates = [current_user]
+        db.session.commit()
+        flash(f"Successfully Joined {group.group_name}")
     return redirect(url_for('group_blueprint.browse_groups'))
 
 @group_blueprint.route('/leave_group/<string:group>')
@@ -95,4 +158,13 @@ def leave_group(group):
     group.mates.remove(current_user)
     db.session.commit()
     flash("Successfully Leave Group")
+    return redirect(url_for('group_blueprint.my_groups'))
+
+@group_blueprint.route('/delete_group/<string:group>')
+def delete_group(group):
+    flash("Delete Group")
+    group = int(''.join(filter(str.isdigit, group)))
+    group = Group.query.get(group)
+    db.session.delete(group)
+    db.session.commit()
     return redirect(url_for('group_blueprint.my_groups'))
